67f89e25b1c8f51c03a0b33668d8c7e7584d2bdb
Victor Mihaita <victormihaita@artlandapp.com>
2020-09-25 16:11:06 +0300

    Initial Commit

---
+++ SwiftUI LearningTests/SwiftUI_LearningTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_LearningTests.swift
+//  SwiftUI LearningTests
+//
+//  Created by Victor Mihaita on 25/09/2020.
+//  Copyright © 2020 Victor Mihaita. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_Learning
+
+class SwiftUI_LearningTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
